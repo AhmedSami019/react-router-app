b0750e8a833e4ea3ff6f46caa12931656d49f4f7
AhmedSami019 <easami019192@gmail.com>
2025-10-20 22:26:52 +0600

    install and setup all elements

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3>This header section</h3>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Components/Root/Root.jsx'
+import Home from './Components/Home/Home.jsx'
+import Laptops from './Components/Laptops/Laptops.jsx'
+import Mobiles from './Components/Moboles/Mobiles.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children:[
+      {index: true, Component: Home},
+      {path: "laptops", Component: Laptops},
+      {path: "mobiles", Component: Mobiles}
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
